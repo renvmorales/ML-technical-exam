64b68cf0b087de3bd7ace583774a252059b51a51
renzo <renvmorales@gmail.com>
2017-10-10 15:43:50 -0300

    Add leave-one-out object and cross-validation loop. Compute the RMSE for each iteration loop. Final estimate is the mean RMSE.

--- regression/q7.py
+++ regression/q7.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import LeaveOneOut
+from sklearn.metrics import mean_squared_error
 
 
 # get data from csv file
@@ -24,12 +25,31 @@ Y = data[:,-1]
 
 
 
-
 model = LinearRegression()
+# model.fit(X, Y)
+# Ypred = model.predict(X)
 
-model.fit(X, Y)
-
-Ypred = model.predict(X)
+# print(Ypred)
 
 
-print(Ypred)
+
+# create a leave-one-out object
+loo = LeaveOneOut()
+
+# create a list to store all rmse values
+rmse = []
+
+
+# Leave-one-out cross-validation loop 
+for train_index, test_index in loo.split(X):
+	X_train, Y_train = X[train_index], Y[train_index]	
+	X_test, Y_test = X[test_index], Y[test_index]
+	model.fit(X_train, Y_train)
+	Ypred = model.predict(X_test)
+	rmse.append( np.sqrt(((Ypred-Y_test)**2).mean()) )
+	# rmse.append(np.sqrt(mean_squared_error(Y_test, Ypred)))
+
+
+# compute mean cross-validation metric
+rmse = np.mean(rmse)
+print('\nLeave-one-out cross-val rmse: %.3f \n' % (rmse))
