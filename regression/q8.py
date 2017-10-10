8f23244ac2806ea5599e680f6762c658b1933734
renzo <renvmorales@gmail.com>
2017-10-10 20:28:07 -0300

    Translate to english directions of question 8

--- regression/q8.py
+++ regression/q8.py
@@ -1,6 +1,7 @@
-# Construa uma árvore de regressão para estimar o valor de Y na tupla 
-# [245,4,9700,4600,1835,?]. Use todos os dados disponíveis e faça com 
-# que a altura máxima da árvore seja igual a 3.
+# Use a regression tree model to estimate an output to the tuple 
+# [245,4,9700,4600,1835]. You should consider all available data, and 
+# a maximum tree-depth equals to 3.
+
 
 
 # import libraries
