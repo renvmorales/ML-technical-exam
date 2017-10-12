560debda0ce31fbe23dbf6a957c1fe493dc2216d
renzo <renvmorales@gmail.com>
2017-10-12 15:21:50 -0300

    Change classification and association-rules folder names

---
+++ classification/q5.py
@@ -0,0 +1,87 @@
+# Classify the tuple [ensolarado, quente, normal, verdadeiro] using 
+# Naïve Bayes classifier.
+
+
+
+# import libraries
+import numpy as np 
+import pandas as pd 
+from sklearn.naive_bayes import GaussianNB
+
+
+
+
+# get datadata from csv file
+df = pd.read_csv('cinco.csv')
+
+
+# convert data to array format
+data = df.as_matrix()
+
+
+# select input and output (nominal data)
+Xnom = data[:,0:-1]
+Ynom = data[:,-1]
+
+
+
+# create array to store numerical-converted data
+X = -1*np.ones(Xnom.shape)
+
+
+
+
+# convert categorical input data to numerical type
+for i in range(Xnom.shape[1]):
+	classes = list(np.unique(Xnom[:,i]))
+	# ls_index = np.arange(0,len(classes))
+	X[:,i] = list(map(lambda x: classes.index(x), list(Xnom[:,i])))
+
+
+
+
+# convert categorical output data to numerical type
+Y = list(map(lambda y: ['Nao', 'Sim'].index(y), list(Ynom)))
+
+
+
+
+
+# create a gaussian Naive Bayes model
+gnb = GaussianNB()
+
+
+
+# train the model with all data
+gnb.fit(X, Y)
+
+
+
+
+# tuple value to predict using the trained model
+xpred = ['Ensolarado', 'Quente', 'Normal', 'Verdadeiro']
+print('\nInput sample: \n', xpred)
+
+
+
+# convert categorical tuple to numerical array
+xpred_nom = []
+for i in range(Xnom.shape[1]):
+	classes = list(np.unique(Xnom[:,i]))
+	xpred_nom.append(classes.index(xpred[i]))
+xpred_nom = np.array(xpred_nom)
+
+
+
+
+# predict the output for given input tuple
+ypred_nom = gnb.predict(np.reshape(xpred_nom, (1,-1)))
+print('\nPredicted value by Naive Bayes classifier: ',
+	['Nao', 'Sim'][int(ypred_nom)])
+
+
+
+
+# compute the probability estimates on the output for the given input
+print('\nProbability estimates on the output:\n ', 
+	gnb.predict_proba([xpred_nom]))
---
+++ classification/q6B.py
@@ -0,0 +1,89 @@
+# Consider the new example xt=[Sim,Sim,Alguns,Italiano] with nominal 
+# attributes. Classify xt according a k-NN algorithm (k=3) with 
+# anti-similarity Hamming metric.
+
+
+
+
+# import libraries
+import numpy as np 
+import pandas as pd 
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+
+# get datadata from csv file
+df = pd.read_csv('seis.csv', sep=';')
+
+
+# convert data to array format
+data = df.as_matrix()
+
+
+# select input and output (nominal data)
+Xnom = data[:,0:-1]
+Ynom = data[:,-1]
+
+
+
+# create array to store numerical-converted data
+X = -1*np.ones(Xnom.shape)
+
+
+
+
+# convert categorical input data to numerical type
+for i in range(Xnom.shape[1]):
+	classes = list(np.unique(Xnom[:,i]))
+	# ls_index = np.arange(0,len(classes))
+	X[:,i] = list(map(lambda x: classes.index(x), list(Xnom[:,i])))
+
+
+
+
+
+# convert categorical output data to numerical type
+Y = list(map(lambda y: ['Nao', 'Sim'].index(y), list(Ynom)))
+
+
+
+
+# create a KNN classifier model object
+neigh = KNeighborsClassifier(n_neighbors=3, metric='hamming')
+
+
+
+
+# train the model with all data
+neigh.fit(X, Y)
+
+
+
+
+
+# tuple value to predict using the trained model
+xpred = ['Sim','Sim','Alguns','Italiano']
+print('\nInput sample: \n', xpred)
+
+
+
+
+
+# convert categorical tuple to numerical array
+xpred_nom = []
+for i in range(Xnom.shape[1]):
+	classes = list(np.unique(Xnom[:,i]))
+	xpred_nom.append(classes.index(xpred[i]))
+xpred_nom = np.array(xpred_nom)
+
+
+
+
+
+
+
+# predict the output for given input tuple
+ypred_nom = neigh.predict([xpred_nom])
+print('\nPredicted value by a Decision Tree classifier: ',
+	['Nao', 'Sim'][int(ypred_nom)])
